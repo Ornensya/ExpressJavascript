693aef862bffa4c89518bf47a6e5533bf6f5ec37
Ornensya <ornensyasembiring@gmail.com>
2023-07-13 11:22:13 +0700

    add code

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ const express = require('express'); //Perintah untuk import library
 const app = express() //Membuat variabel baru untuk menampung library express
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-// Menambah function baru di file js
-const arrow = function(res) {
-   return res("Hello world")
-}
-console.log(arrow(res));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // app.listen(port, () => {
