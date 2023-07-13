38e29e0746851ae73fa8f9140248a1acc7b6a829
Ornensya <ornensyasembiring@gmail.com>
2023-07-14 02:50:53 +0700

    add file connection

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express'); //Perintah untuk import library
-
 const app = express() //Membuat variabel baru untuk menampung library express
 const port = 3000
+const pool = require('./db/connection');
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-app.get(`/`, function(req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM tabelornensya', (error, results)=> {
+    if (error) {
+      throw error;
+    }
+    res.send(results.row);
+  });
 })
 
+// app.get(`/`, function(req, res) {
+//   res.send('Hello World!');
+// })
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
 })
